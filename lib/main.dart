731f0be6965d2f514b26b9103bcaf4a75e51e9ca
アリメクタ アレネクト <alimektor@gmail.com>
2025-11-17 08:08:48 +0300

    feat: Update to lesson 76

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(onPressed: () {
+              final AudioPlayer player = AudioPlayer();
+              player.play(AssetSource('note1.wav'));
+            }, child: Text("Click Me"),),
+          ),
         ),
       ),
     );
